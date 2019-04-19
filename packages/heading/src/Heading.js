3ce3ffb760d83803c9f558df6c8ad891607ff823
ttnguyen <tuannguyen.tran@orientsoftware.com>
2019-04-19 17:15:19 +0700

    update

--- packages/heading/src/Heading.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Heading {
-    get(id) {
-        return {};
-    }
-}
-exports.Heading = Heading;
-//# sourceMappingURL=Heading.js.map
