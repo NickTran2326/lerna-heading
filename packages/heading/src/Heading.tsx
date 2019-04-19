d9f826f4ec33fa01b9db3faa3ecacfa1290362ac
ttnguyen <tuannguyen.tran@orientsoftware.com>
2019-04-19 15:43:31 +0700

    update code

--- packages/heading/src/Heading.tsx
+++ packages/heading/src/Heading.tsx
@@ -1,5 +1,13 @@
-export class Heading {
-    get(id) {
-        return {};
+import React, { Component } from 'react';
+
+class Heading extends Component {
+    render() {
+        return (
+            <div>
+                <p>Nothing at here!</p>
+            </div>
+        );
     }
 }
+
+export default Heading;
