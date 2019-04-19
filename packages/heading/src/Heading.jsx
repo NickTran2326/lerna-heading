b3ade0adedb2b0fe880b0337a3bda8137f214c2d
ttnguyen <tuannguyen.tran@orientsoftware.com>
2019-04-19 17:23:33 +0700

    update

--- packages/heading/src/Heading.jsx
+++ packages/heading/src/Heading.jsx
@@ -4,7 +4,7 @@ const react_1 = require("react");
 class Heading extends react_1.Component {
     render() {
         return (<div>
-                <p>Nothing at here!</p>
+                <p>Nothing at here! hehe</p>
             </div>);
     }
 }
